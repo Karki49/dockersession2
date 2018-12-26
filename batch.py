22cdc150ad6977d55528d70e7cde89d78b4260cc
Aayush Karki <akarki@tekvortex.com>
2018-12-26 15:06:49 +0545

    minor fix

--- batch.py
+++ batch.py
@@ -8,7 +8,7 @@ import os
 def makeCake(customerName):
     cakeType = random.choice(["black forest", "while forest", "caramel"])
     for step in ["making batter", "put in oven", "let it cool"]:
-        logging.info(step + " Cake for " + customerName)
+        logging.info(step + ". Cake for " + customerName)
         time.sleep(5)
     logging.info("Tada! %s cake for %s is ready!" %(cakeType, customerName))
 
